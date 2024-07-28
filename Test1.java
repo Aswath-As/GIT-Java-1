80b4a1c0329611a0967b5be04f728d9ca4b8178b
Aswath-As <aswath200411@gmail.com>
2024-07-28 11:54:12 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,14 @@
+package aswathjava;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+}
